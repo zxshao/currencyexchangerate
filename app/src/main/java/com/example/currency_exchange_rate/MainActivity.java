8b34d6bc4055ad3ccd5800c3c67d113c657a5198
zxshao <zixuans2@illinois.edu>
2019-04-23 17:58:12 -0500

    function

--- app/src/main/java/com/example/currency_exchange_rate/MainActivity.java
+++ app/src/main/java/com/example/currency_exchange_rate/MainActivity.java
@@ -117,6 +117,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        return;
     }
 }
---
+++ app/src/main/java/com/example/currency_exchange_rate/exchange.java
@@ -0,0 +1,14 @@
+package com.example.currency_exchange_rate;
+
+import com.google.gson.JsonObject;
+
+public class exchange {
+    public double calculate(JsonObject json, String outCurrency, double inputNumber) {
+        try {
+            double rate = json.getAsJsonObject("rates").getAsJsonObject(outCurrency).getAsDouble();
+            return rate * inputNumber;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
